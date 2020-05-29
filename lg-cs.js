901eb0a319d59e7149333e13074e1f162017e2e5
Hundter Biede <6586509+hbiede@users.noreply.github.com>
2020-05-29 01:01:45 -0500

    Add default turns

--- lg-cs.js
+++ lg-cs.js
@@ -4,7 +4,7 @@
 // @namespace       hbiede.com
 // @description     Set the straight options by default on new lane guidance changes
 // @include         /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor.*$/
-// @version         2020.04.29.002
+// @version         2020.05.29.001
 // @grant           none
 // @license         MIT
 // @copyright       2020 HBiede
@@ -14,30 +14,41 @@
 
 setTimeout(initLaneGuidanceClickSaver, 1000);
 
-function setStraightTurns() {
-    let turnSections = document.getElementsByClassName("turn-lane-edit");
-    for (let i = 0; i < turnSections.length; i++) {
-        if (turnSections[i].getElementsByClassName("angle-0").length > 0) {
-            // Set all lanes for straight turns
-            let laneCheckboxes = turnSections[i].getElementsByTagName("input");
+function setTurns() {
+    let lanesPane = document.getElementsByClassName("lanes")[0];
+    let left = (lanesPane.getElementsByClassName("angle--135").length > 0 ? "angle--135"
+                      : (lanesPane.getElementsByClassName("angle--90").length > 0 ? "angle--90" : "angle--45"));
+    let right = (lanesPane.getElementsByClassName("angle-135").length > 0 ? "angle-135"
+                      : (lanesPane.getElementsByClassName("angle-90").length > 0 ? "angle-90" : "angle-45"));
 
-            // Check if the lanes are already set. If already set, don't change anything
-            let alreadySet = 0;
-            for (let j = 0; j < laneCheckboxes.length && !alreadySet; j++) {
-                if (laneCheckboxes[j].checked !== undefined) {
-                    if (laneCheckboxes[j].checked === true) {
-                        alreadySet++;
-                    }
+    let turnSections = lanesPane.getElementsByClassName("turn-lane-edit");
+    for (let i = 0; i < turnSections.length; i++) {
+        // Check if the lanes are already set. If already set, don't change anything
+        let alreadySet = 0;
+        let laneCheckboxes = turnSections[i].getElementsByTagName("input");
+        for (let j = 0; j < laneCheckboxes.length && !alreadySet; j++) {
+            if (laneCheckboxes[j].checked !== undefined) {
+                if (laneCheckboxes[j].checked === true) {
+                    alreadySet++;
                 }
             }
-
-            if (alreadySet === 0) {
+        }
+        if (alreadySet === 0 && laneCheckboxes && laneCheckboxes.length > 0) {
+            if (turnSections[i].getElementsByClassName("angle-0").length > 0) {
+                // Set all lanes for straight turns
                 for (let j = 0; j < laneCheckboxes.length; j++) {
                     if (laneCheckboxes[j].checked !== undefined && laneCheckboxes[j].checked === false) {
                         laneCheckboxes[j].click();
-                        console.log(j);
                     }
                 }
+            } else if (turnSections[i].getElementsByClassName(left).length > 0
+                       && laneCheckboxes[0].checked !== undefined
+                       && laneCheckboxes[0].checked === false) {
+                laneCheckboxes[0].click();
+            } else if (turnSections[i].getElementsByClassName(right).length > 0
+                       && laneCheckboxes[laneCheckboxes.length - 1].checked !== undefined
+                       && laneCheckboxes[laneCheckboxes.length - 1].checked === false) {
+                laneCheckboxes[laneCheckboxes.length - 1].click();
             }
         }
     }
@@ -65,7 +76,7 @@ function initLaneGuidanceClickSaver() {
                     'change',
                     function() {
                         // wait for the input to appear
-                        setTimeout(setStraightTurns(), 50);
+                        setTimeout(setTurns(), 50);
                     },
                     false
                 );
@@ -77,7 +88,7 @@ function initLaneGuidanceClickSaver() {
                     'click',
                     function() {
                         // wait for the input to appear
-                        setTimeout(setStraightTurns(), 50);
+                        setTimeout(setTurns(), 50);
                     },
                     false
                 );
